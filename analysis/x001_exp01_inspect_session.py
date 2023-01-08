57bd3136e9c069349136a8f71e26e989cc422892
Mohammad Shams <m.shamsahmar@gmail.com>
2023-01-07 19:58:21 -0500

    analyzed the very first version of the experiment 1 (run myself) and sent to Patrick

--- analysis/x001_exp01_inspect_session.py
+++ analysis/x001_exp01_inspect_session.py
@@ -10,14 +10,54 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from lib import cleanplot as cp
 from matplotlib.patches import Polygon
+
 
 # ----------------------------------------------------------------------------
 
-# /// LOAD DATA ///
+# /// FUNCTIONS ///
+
+def modify_maps(ax, contour=False, contour_im=None):
+    vertices = np.array([(0 - 2.5, 5 - 2.5),
+                         (0 + 2.5, 5 - 2.5),
+                         (0 + 2.5, 5 + 2.5),
+                         (0 - 2.5, 5 + 2.5)])
+    poly = Polygon(vertices, fc='none', ec='black', lw=2)
+    ax.add_patch(poly)
+    ax.plot(0, 0, '+', markersize=15, color='black')
+    cp.remove_box(ax)
+    if contour:
+        for c in contour_im.collections:
+            c.set_edgecolor("face")
+
+
+def limit_large(ax):
+    ax.set(xlim=[-6, 9], ylim=[-3, 12])
+
+
+def limit_small(ax):
+    ax.set(xlim=[-5, 8], ylim=[-1.5, 11.5])
+
+
+def modify_upper_ax(ax):
+    ax.set(xlim=[-5, 8])
+    cp.remove_box(ax)
+
+
+def modify_side_ax(ax):
+    ax.set(ylim=[-1.5, 11.5])
+    cp.remove_box(ax)
+
+
+# ----------------------------------------------------------------------------
+
+# /// CONFIGURE PATHS AND LOAD DATA ///
 
 data_dir = os.path.join('..', 'data', 'raw')
+save_dir = os.path.join('..', 'results')
 file_name = "MS1_20230106_154600.json"
+subID = file_name[:3]
 file_address = os.path.join(data_dir, file_name)
 df = pd.read_json(file_address)
 # ----------------------------------------------------------------------------
@@ -25,8 +65,8 @@ df = pd.read_json(file_address)
 # /// CONFIGURE DATA ///
 
 # /// flip the data in leftward codition
-msk_rightdir = df['motion_dir'] == 1
-msk_leftdir = df['motion_dir'] == -1
+msk_rightdir = df['movobj_dir'] == 1
+msk_leftdir = df['movobj_dir'] == -1
 
 click_locs = df.click_loc.apply(pd.Series)
 probe_locs = df.probe_loc.apply(pd.Series)
@@ -37,11 +77,12 @@ probe_locs.loc[msk_leftdir, 0] = -probe_locs.loc[msk_leftdir, 0]
 # /// calculate click errors
 errs = click_locs - probe_locs
 
-# /// create the map
-# find unique x and y positions of the probe
-k = 9
-err_map_x = np.full((k, k), fill_value=np.nan)
-err_map_y = np.full((k, k), fill_value=np.nan)
+# /// create the error arrays
+k = 81
+probe_arr_x = np.full(k, fill_value=np.nan)
+probe_arr_y = np.full(k, fill_value=np.nan)
+err_arr_x = np.full(k, fill_value=np.nan)
+err_arr_y = np.full(k, fill_value=np.nan)
 probe_xs = probe_locs[0]
 probe_xs = np.unique(probe_xs)
 probe_xs.sort()
@@ -49,48 +90,131 @@ probe_ys = probe_locs[1]
 probe_ys = np.unique(probe_ys)
 probe_ys.sort()
 probe_ys = probe_ys[::-1]
+pcnt = -1
+for probe_x in probe_xs:
+    for probe_y in probe_ys:
+        pcnt += 1
+        ind_loc = (probe_locs[0] == probe_x) & (probe_locs[1] == probe_y)
+        probe_arr_x[pcnt] = np.mean(probe_locs[ind_loc], axis=0)[0]
+        probe_arr_y[pcnt] = np.mean(probe_locs[ind_loc], axis=0)[1]
+        err_arr_x[pcnt] = np.mean(errs[ind_loc], axis=0)[0]
+        err_arr_y[pcnt] = np.mean(errs[ind_loc], axis=0)[1]
+
+# /// create the error maps
+k = (9, 9)
+probe_map_x = np.full(k, fill_value=np.nan)
+probe_map_y = np.full(k, fill_value=np.nan)
+herr_map = np.full(k, fill_value=np.nan)
+verr_map = np.full(k, fill_value=np.nan)
 for i, probe_x in enumerate(probe_xs):
     for j, probe_y in enumerate(probe_ys):
         ind_loc = (probe_locs[0] == probe_x) & (probe_locs[1] == probe_y)
-        err_map_x[j, i] = np.mean(errs[ind_loc], axis=0)[0]
-        err_map_y[j, i] = np.mean(errs[ind_loc], axis=0)[1]
+        probe_map_x[j, i] = np.mean(probe_locs[ind_loc], axis=0)[0]
+        probe_map_y[j, i] = np.mean(probe_locs[ind_loc], axis=0)[1]
+        herr_map[j, i] = np.mean(errs[ind_loc], axis=0)[0]
+        verr_map[j, i] = -np.mean(errs[ind_loc], axis=0)[1]
 
 # /// calculate mislocalization along each axis
-# horizontal error along x axis
-herr_x = np.mean(err_map_x, 0)
-# horizontal error along y axis
-herr_y = np.mean(err_map_x, 1)
-# vertical error along x axis
-verr_x = np.mean(err_map_y, 0)
-# vertical error along y axis
-verr_y = np.mean(err_map_y, 1)
+# horizontal error along x axis (>0: towards motion)
+herr_x = np.mean(herr_map, 0)
+# horizontal error along y axis (>0: towards motion)
+herr_y = np.mean(herr_map, 1)
+# vertical error along x axis (>0: towards fixation)
+verr_x = np.mean(verr_map, 0)
+# vertical error along y axis (>0: towards fixation)
+verr_y = np.mean(verr_map, 1)
 # ----------------------------------------------------------------------------
 
 # @@@ PLOT RESULTS @@@
 
+# /// set plot parameters
+cp.prep4ai()
+ncontours = 100
+# --------------------------------------
+
 # @@@ map of probes and clicks
-_, axs = plt.subplots(1, figsize=(7, 4))
-axs.plot(click_locs[0], click_locs[1],
-         '.', color='black')
-axs.plot(probe_locs[0], probe_locs[1],
-         'o', mec='red', mfc='none')
-axs.set(xlim=[-12, 12], ylim=[-3, 12])
-vertices = np.array([(0 - 2.5, 5 - 2.5),
-                     (0 + 2.5, 5 - 2.5),
-                     (0 + 2.5, 5 + 2.5),
-                     (0 - 2.5, 5 + 2.5)])
-poly = Polygon(vertices, facecolor='none', edgecolor='black', alpha=.5)
-axs.add_patch(poly)
+fig, axs = plt.subplots(1, 2, figsize=(8, 4))
+fig.suptitle(f"subID: {subID} – Probes, Clicks, and Error Map")
+axs[0].plot(click_locs[0], click_locs[1],
+            '.', color='grey')
+axs[0].plot(probe_locs[0], probe_locs[1],
+            'o', mec='red', mfc='none')
+axs[0].set(xlabel='Horizontal position wrt fixation [deg]',
+           ylabel='Vertical position wrt fixation [deg]')
+limit_large(axs[0])
+modify_maps(axs[0])
 
-# @@@ mislocalization map
-fig, axs = plt.subplots(1, figsize=(5, 5))
-im = axs.imshow(err_map_x, cmap='copper', vmin=-2.5, vmax=2.5)
-fig.colorbar(im, ax=axs)
+axs[1].quiver(probe_arr_x, probe_arr_y,
+              err_arr_x, err_arr_y,
+              color='grey', width=0.004)
+axs[1].set(xlabel='Horizontal position wrt fixation [deg]',
+           ylabel='Vertical position wrt fixation [deg]')
+limit_large(axs[1])
+modify_maps(axs[1])
 
-# @@@ mislocalization along each axis
-_, axs = plt.subplots(2, 2, figsize=(7, 7))
-axs[0, 0].plot(herr_x)
-axs[0, 1].plot(herr_y)
-axs[1, 0].plot(verr_x)
-axs[1, 1].plot(verr_y)
+plt.savefig(os.path.join(save_dir, f"{subID}_probes_clicks_errors.pdf"))
+# --------------------------------------
 
+# @@@ horizontal mislocalization map
+fig, axs = plt.subplots(3, 2, figsize=(5.5, 6),
+                        gridspec_kw={'width_ratios': [4, 1],
+                                     'height_ratios': [1, 4, .5]})
+fig.suptitle(f"subID: {subID} – Vertical Misloc. Map")
+im = axs[1, 0].contourf(probe_map_x, probe_map_y, herr_map,
+                        levels=ncontours, cmap='viridis')
+limit_small(axs[1, 0])
+modify_maps(axs[1, 0])
+cbar_ax = axs[1, 0].inset_axes([0, -.2, 1, .02])
+fig.colorbar(im, cax=cbar_ax, orientation='horizontal')
+cbar_ax.set(xticks=[0, .5, 1, 1.5])
+axs[1, 0].set(xlabel='Horizontal position wrt fixation [deg]',
+              ylabel='Vertical position wrt fixation [deg]')
+limit_small(axs[1, 0])
+modify_maps(axs[1, 0], contour=True, contour_im=im)
+
+axs[0, 0].plot(probe_xs, herr_x, color='black')
+axs[0, 0].set(ylabel='Misloc. towards fixation [deg]')
+modify_upper_ax(axs[0, 0])
+
+axs[1, 1].plot(herr_y, probe_ys, color='black')
+axs[1, 1].set(xlabel='Misloc. towards fixation [deg]')
+modify_side_ax(axs[1, 1])
+
+axs[0, 1].axis('off')
+axs[2, 0].axis('off')
+axs[2, 1].axis('off')
+
+plt.savefig(os.path.join(save_dir, f"{subID}_horizontal_err_map.pdf"))
+# --------------------------------------
+
+# @@@ vertical mislocalization map
+fig, axs = plt.subplots(3, 2, figsize=(5.5, 6),
+                        gridspec_kw={'width_ratios': [4, 1],
+                                     'height_ratios': [1, 4, .5]})
+fig.suptitle(f"subID: {subID} – Vertical Misloc. Map")
+im = axs[1, 0].contourf(probe_map_x, probe_map_y, verr_map,
+                        levels=ncontours, cmap='viridis')
+limit_small(axs[1, 0])
+modify_maps(axs[1, 0])
+cbar_ax = axs[1, 0].inset_axes([0, -.2, 1, .02])
+fig.colorbar(im, cax=cbar_ax, orientation='horizontal')
+cbar_ax.set(xticks=[0, .5, 1, 1.5, 2])
+axs[1, 0].set(xlabel='Horizontal position wrt fixation [deg]',
+              ylabel='Vertical position wrt fixation [deg]')
+limit_small(axs[1, 0])
+modify_maps(axs[1, 0], contour=True, contour_im=im)
+
+axs[0, 0].plot(probe_xs, verr_x, color='black')
+axs[0, 0].set(ylabel='Misloc. towards fixation [deg]')
+modify_upper_ax(axs[0, 0])
+
+axs[1, 1].plot(verr_y, probe_ys, color='black')
+axs[1, 1].set(xlabel='Misloc. towards fixation [deg]')
+modify_side_ax(axs[1, 1])
+
+axs[0, 1].axis('off')
+axs[2, 0].axis('off')
+axs[2, 1].axis('off')
+
+plt.savefig(os.path.join(save_dir, f"{subID}_vertical_err_map.pdf"))
+# --------------------------------------
---
+++ lib/cleanplot.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib as plt
+from matplotlib.patches import Polygon
+
+
+def remove_box(ax):
+    # removes the upper and the right borders of the axis
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+
+def prep4ai():
+    plt.rcParams['pdf.fonttype'] = 42  # AI can detect text now
+    plt.rcParams['font.sans-serif'] = "Arial"

fb79b2cd3e29139b2ead447d2992e7678a056626
Mohammad Shams <m.shamsahmar@gmail.com>
2023-01-10 18:49:12 -0500

    changed the default font size

--- lib/cleanplot.py
+++ lib/cleanplot.py
@@ -12,3 +12,4 @@ def remove_box(ax):
 def prep4ai():
     plt.rcParams['pdf.fonttype'] = 42  # AI can detect text now
     plt.rcParams['font.sans-serif'] = "Arial"
+    plt.rcParams['font.size'] = 8

f596f85178a1a4628828b06c36dc7f0a9a31e539
Mohammad Shams <m.shamsahmar@gmail.com>
2023-01-19 17:33:15 -0500

    added a get_mouse function specifically for experiment 5

--- lib/keymouse.py
+++ lib/keymouse.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from psychopy import event, core
 
 
@@ -15,6 +16,21 @@ def get_mouseclick(win):
     mouse = event.Mouse(win=win, visible=True,
                         newPos=[ms_posx * ms_corrcoef,
                                 ms_posy * ms_corrcoef])
+    while not mouse.getPressed()[0]:
+        escape_session()  # force exit with 'escape' button
+        win.flip()
+    while mouse.getPressed()[0]:
+        pass
+    click_loc = mouse.getPos() / ms_corrcoef
+    click_loc = [round(item, 2) for item in click_loc]
+    mouse.setVisible(False)
+    return click_loc
+
+
+def get_mouseclick_exp5(win, pos):
+    ms_corrcoef = 2
+    pos = np.array(pos) * ms_corrcoef
+    mouse = event.Mouse(win=win, visible=True, newPos=pos)
     while not mouse.getPressed()[0]:
         escape_session()  # force exit with 'escape' button
         win.flip()

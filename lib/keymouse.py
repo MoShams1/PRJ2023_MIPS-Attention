356e4fad9b1c68d4c1f3c74b169c703ec8e8c45b
MShams <m.shamsahmar@gmail.com>
2023-02-24 21:10:24 +0100

    working on four experiments for ECVP abstract submission

--- lib/keymouse.py
+++ lib/keymouse.py
@@ -40,3 +40,20 @@ def get_mouseclick_exp5(win, pos):
     click_loc = [round(item, 2) for item in click_loc]
     mouse.setVisible(False)
     return click_loc
+
+
+def get_mouseclick11(win, ms_corrcoef=1):
+    ms_posx = random.choice(range(-3, 3 + 1))
+    ms_posy = random.choice(range(-1, 1 + 1))
+    mouse = event.Mouse(win=win, visible=True,
+                        newPos=[ms_posx * ms_corrcoef,
+                                ms_posy * ms_corrcoef])
+    while not mouse.getPressed()[0]:
+        escape_session()  # force exit with 'escape' button
+        win.flip()
+    while mouse.getPressed()[0]:
+        pass
+    click_loc = mouse.getPos() / ms_corrcoef
+    click_loc = [round(item, 2) for item in click_loc]
+    mouse.setVisible(False)
+    return click_loc
--- stimulus/explore/explore11_circPath.py
+++ stimulus/explore/explore11_circPath.py
@@ -4,14 +4,11 @@ Mo Shams <MShamsCBR@gmail.com>
 Initiated on: Feb 20, 2023
 ---
 
-SPATIAL PROFILE OF THE ANISOTROPY
+SPATIAL PROFILE OF THE ANISOTROPY (LOCALLY PREDICTABLE FLASH)
 
 - A bar rotates around the fixation dot.
-- The path and the phase of the trajectory is constant.
+- The path, direction, and the phase of the trajectory is constant.
 - When the bar is at top, a probe flashes at different positions wrt the bar.
-- The time of the flash is constant and predictable according to the bar's
-trajectory.
-- The position of the flash is not predictable.
 
 """
 
@@ -26,9 +23,11 @@ from lib import config_visual as cvis, genpath, keymouse, timestamp
 # /// GENERAL SETTINGS ///
 
 subID = 'test'
-NTRIALS = 1
-screen_num = 1  # 0: primary    1: secondary
-frame_rate = 120
+n_tests_per_position = 3
+test_grid_width_n = 5
+ntrials = n_tests_per_position * test_grid_width_n * test_grid_width_n
+screen_num = 0  # 0: primary    1: secondary
+frame_rate = 60
 full_screen = False
 
 command_keys = {'quit_key': 'escape', 'response_key': 'space'}
@@ -36,7 +35,7 @@ command_keys = {'quit_key': 'escape', 'response_key': 'space'}
 
 # /// SET UP DIRECTORY PATHS ///
 
-save_dir = os.path.join('../..', 'data', 'raw')
+save_dir = os.path.join('..', '..', 'data', 'explore11')
 file_name = f"{subID}_{timestamp.getdate()}_{timestamp.gettime()}.json"
 save_address = os.path.join(save_dir, file_name)
 # ----------------------------------------------------------------------------
@@ -67,54 +66,76 @@ fixdot_dur = 1 * practical_fr  # sec x Hz = frames
 movobj_size = [.15, 3]
 movobj_color = 'white'
 movobj_path_radius = 5
-movobj_theta_first = 270
-movobj_theta_last = movobj_theta_first + 360
-movobj_dur_sec = 2
-movobj_dur = int(movobj_dur_sec * practical_fr)-1  # sec x Hz = frames
+# movobj_theta_first = 270
+movobj_theta_first = random.choice(range(180, 360, 10))
+movobj_dur_sec = 1
+movobj_dur = int(movobj_dur_sec * practical_fr) - 1  # sec x Hz = frames
 # make sure movobj_dur is a factor of 3 and an odd number
 assert movobj_dur % 2 == 1, 'Number of frames is not an odd number.'
 
 movobj_atflash = None
-movobj_thetas = genpath.angular(theta1=movobj_theta_first,
-                                theta2=movobj_theta_last,
-                                dur=movobj_dur)
-
-# /// test grid
-# grid_width = 12
-# grid_n = NGRIDS
 
 # /// flashing object(s)
-probe_rad = .4  # radius of the probe
+probe_rad = .25  # radius of the probe
 probe_color = 'red'
+# probe_frame_offset_range = 0
 probe_frame = int(movobj_dur / 2)  # frame num where the probe flashes
-
+probe_frame_offset_coeff = 0.25  # proportion tolerance to deviate from midway
+probe_frame1 = 0
+probe_frame2 = int(movobj_dur * 2 * probe_frame_offset_coeff)
+# generate test grid
+grid_x, grid_y = cvis.gengrid3(width=4, n=[5, 5], pos=[0, movobj_path_radius])
 # generate probe positions
-probe_xpos_list = np.arange(-2, 2, .5)
-probe_ypos = 5
+grid_x_arr = grid_x.flatten()
+grid_y_arr = grid_y.flatten()
+probe_pos_temp = list(zip(grid_x_arr, grid_y_arr))
+probe_pos_list = []
+for itest in range(n_tests_per_position):
+    probe_pos_list = probe_pos_list + probe_pos_temp
+random.shuffle(probe_pos_list)
 # ----------------------------------------------------------------------------
 
 # /// CONFIGURE MONITOR ///
 
-mon = cvis.configmon_imac()
+mon = cvis.configmon_dell()
 win = cvis.configwin(mon=mon, screen=screen_num,
                      fullscr=full_screen,
                      color=bg_color)
-# cvis.test_framerate(win=win, nominal_fr=frame_rate)
+cvis.test_framerate(win=win, nominal_fr=frame_rate)
 # ----------------------------------------------------------------------------
 
 # /// START TRIAL ///
 
-for itrial in range(NTRIALS):
+for itrial in range(3):
+
     # -------------------------------
 
     # /// set up trial variables
+
+    # decide on rotating direction of the moving bar
+    movobj_dir = 'cw'  # 'cw' or 'ccw'
+    movobj_thetas = genpath.angular(theta1=movobj_theta_first,
+                                    dur=movobj_dur,
+                                    rotdir=movobj_dir)
+
+    # decide on position of the bar when the probe flashes
+    probe_frame_offset = random.choice(range(probe_frame1, probe_frame2))
+    probe_frame = probe_frame + probe_frame_offset
 
     # decide on gap durations
     firstgap_dur = np.random.choice(gap_dur_arr)
     lastgap_dur = np.random.choice(gap_dur_arr)
 
-    # decide on the motion direction and adjust motion path and flash position
-    probe_xpos_trial = random.choice(probe_xpos_list)
+    # adjust flash position
+    probe_pos_trial = probe_pos_list[itrial]
+    theta = movobj_thetas[probe_frame]
+    theta_rad = theta / 360 * 2 * np.pi
+    probe_pos_trial = \
+        cvis.rotate_point(origin=(movobj_path_radius * np.cos(theta_rad),
+                                  movobj_path_radius * np.sin(theta_rad)),
+                          point=probe_pos_trial,
+                          angle=theta_rad,
+                          rotdir=movobj_dir)
     # -------------------------------
 
     # /// run task
@@ -137,43 +158,37 @@ for itrial in range(NTRIALS):
             if iframe == probe_frame:
                 cvis.addprobe2(win=win, radius=probe_rad,
                                color=probe_color,
-                               pos=(-2, probe_ypos))
+                               pos=probe_pos_trial)
+                movobj_atflash = movobj_thetas[iframe]
             win.flip()
 
     # response period
-    # click_loc = keymouse.get_mouseclick(win)
-
+    click_loc = keymouse.get_mouseclick11(win)
     # gap period
     for frame in range(lastgap_dur):
         win.flip()
 
     # -------------------------------
-    #
-    # # /// save data
-    #
-    # # create a dictionary
-    # trial_dict = {'trial_num': [itrial + 1],
-    #               'probe_loc': [probe_pos_tr],
-    #               'click_loc': [click_loc],
-    #               'movobj_flashpos': [movobj_atflash],
-    #               'movobj_size': [movobj_size],
-    #               'movobj_dur': [round(movobj_dur / practical_fr, 2)],
-    #               'movobj_firstpos': [(movobj_pathx_tr[0],
-    #                                    movobj_pathy_tr[0])],
-    #               'movobj_lastpos': [(movobj_pathx_tr[-1],
-    #                                   movobj_pathy_tr[-1])],
-    #               'movobj_dir': movobj_dir,
-    #               'gap_dur': [round(firstgap_dur / practical_fr, 2)]}
-    #
-    # # convert to data frame
-    # dfnew = pd.DataFrame(trial_dict)
-    #
-    # # if first trial create a file, else load and add the new data frame
-    # if itrial == 0:
-    #     dfnew.to_json(save_address)
-    # else:
-    #     df = pd.read_json(save_address)
-    #     dfnew = pd.concat([df, dfnew], ignore_index=True)
-    #     dfnew.to_json(save_address)
+
+    # /// save data
+
+    # create a dictionary
+    trial_dict = {'trial_num': [itrial + 1],
+                  'probe_loc': [probe_pos_trial],
+                  'click_loc': [click_loc],
+                  'movobj_atflash': [movobj_atflash],
+                  'movobj_dur_sec': [movobj_dur_sec],
+                  'movobj_dir': [movobj_dir]}
+
+    # convert to data frame
+    dfnew = pd.DataFrame(trial_dict)
+
+    # if first trial create a file, else load and add the new data frame
+    if itrial == 0:
+        dfnew.to_json(save_address)
+    else:
+        df = pd.read_json(save_address)
+        dfnew = pd.concat([df, dfnew], ignore_index=True)
+        dfnew.to_json(save_address)
 
 win.close()

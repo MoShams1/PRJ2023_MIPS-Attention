8ca82603320ea6dc70b86824e158c1346fa57144
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-06-01 11:52:08 -0400

    preparing for the pilot run

--- stimulus/ECVP23_Exp03.py
+++ stimulus/ECVP23_Exp03.py
@@ -24,7 +24,7 @@ from lib import config_visual as cvis, genpath, keymouse, timestamp
 
 # /// GENERAL SETTINGS ///
 
-subID = '2002'
+subID = '0001'
 n_tests_per_position = 5
 screen_num = 0  # 0: primary    1: secondary
 frame_rate = 60

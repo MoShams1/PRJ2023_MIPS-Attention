9221e74389259bfccb6c7aa66d9c3e35801cbfba
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-08-14 10:54:17 -0400

    tested the timing and mouse click positions on Linux

--- stimulus/cyc02_task02.py
+++ stimulus/cyc02_task02.py
@@ -37,7 +37,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # /// GENERAL SETTINGS ///
 
-subID = 'test'
+subID = '2033'
 rep_per_cnd = 12  # repetition per condition
 full_screen = True
 running_device = 'linux'  # 'linux' or 'mac'

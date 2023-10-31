7a0c96cef8301bc6ac44d5fe6259711555d99fca
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-10-31 11:50:29 -0400

    ran AS01 on task 6 version 2 (fair)

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -52,7 +52,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'HA01'
+subID = 'AS01'
 nrep = 50
 ndir = 2
 ntrs = nrep * ndir

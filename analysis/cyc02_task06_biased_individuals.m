c3da929bc6cbc8e376688ee99851b180aa81b95b
Mohammad Shams <m.shamsahmar@gmail.com>
2023-11-01 13:18:19 -0400

    new analyses

---
+++ analysis/cyc02_task06_biased_individuals.m
@@ -0,0 +1,109 @@
+clc
+clear
+% close all
+
+% Specify the path to the JSON file
+
+jsonFilePath = '../data/cyc02/MS01_task06_biased_20231026_154242.json';
+
+
+% Open the JSON file and read its content
+fileID = fopen(jsonFilePath);
+jsonContent = fread(fileID, '*char')';
+fclose(fileID);
+
+% Parse the JSON content
+jsonData = jsondecode(jsonContent);
+
+% convert structure to arrays
+dir1 = cell2mat(struct2cell(jsonData.preflash_dir));
+dir2 = struct2cell(jsonData.postflash_dir);
+clk_xerr = cell2mat(struct2cell(jsonData.click_xerr));
+clk_yerr = cell2mat(struct2cell(jsonData.click_yerr));
+
+% create data cell
+ind_v = strcmp(dir2, 'v');
+ind_h = strcmp(dir2, 'h');
+err_cell{1} = clk_xerr(ind_v & dir1>0);
+err_cell{2} = clk_xerr(ind_h & dir1>0);
+err_cell{3} = clk_xerr(ind_v & dir1<0);
+err_cell{4} = clk_xerr(ind_h & dir1<0);
+err_cell{5} = clk_yerr(ind_v & dir1>0);
+err_cell{6} = clk_yerr(ind_h & dir1>0);
+err_cell{7} = clk_yerr(ind_v & dir1<0);
+err_cell{8} = clk_yerr(ind_h & dir1<0);
+
+labels = {'RU','RR','LU','LL'};
+ntypes = numel(labels);
+
+%% 2D plot
+figure('units','normalized','outerposition',[.1 .3 .3 .5])
+hold on
+
+alpha = .5;
+
+h1 = scatter(err_cell{:,1},err_cell{:,5},'b','o','fill','markerfacealpha',alpha);
+h2 = scatter(err_cell{:,2},err_cell{:,6},'b','o');
+h3 = scatter(err_cell{:,3},err_cell{:,7},'r','o','fill','markerfacealpha',alpha);
+h4 = scatter(err_cell{:,4},err_cell{:,8},'r','o');
+
+xline(0)
+xlim([-1 1] * 1.6)
+xticks(-1:.25:1)
+xlabel 'Horizontal click error (dva)'
+
+yline(0)
+ylim([-1 1] * 1.6)
+yticks(-1:.25:1)
+ylabel 'Vertical click error (dva)'
+
+legend([h1 h2 h3 h4],labels)
+
+cleanplot
+
+%% scatterbar plot
+figure('units','normalized','outerposition',[.4 .3 .25 .5])
+hold on
+
+scatterbar(err_cell([1,2,5,6]), 30, 'b');
+scatterbar(err_cell([3,4,7,8]), 30, 'r');
+
+xlim([.5 ntypes+.5])
+xticks(1:ntypes)
+xticklabels({'Vx','Hx','Vy','Hy'})
+xlabel 'Direction conditions'
+
+ylim([-1 1] * 1.6)
+yticks(-1:.25:1)
+yline(0)
+ylabel 'Click error (dva)'
+
+title 'Raw data'
+
+text(1,1.2,'Pre-Flash Rightward Motion','color','b')
+text(1,1.1,'Pre-Flash Leftward Motion','color','r')
+cleanplot
+
+%%
+function scatterbar(A,marksz,color)
+% A: a cell of cetegories
+
+ncat    = numel(A); % number of categories
+stdx    = .05; % standard deviation of scatters in each category
+linelm  = .3; % line length for median
+alpha = .3;
+
+hold on
+for icat = 1:ncat    
+    rng default
+    n = numel(A{icat});
+    x = randn(n,1)*stdx + icat;
+    
+    scatter(x,A{icat},marksz,color,'o','fill','markerfacealpha',alpha);
+    line([icat-linelm icat+linelm],[nanmedian(A{icat}) nanmedian(A{icat})],...
+        'color',color,'linewidth',2);
+end
+
+xlim([0 ncat+1])
+set(gca,'xtick',1:ncat)
+end
---
+++ analysis/cyc02_task06_v2_individuals.m
@@ -0,0 +1,89 @@
+clc
+clear
+% close all
+
+% Specify the path to the JSON file
+
+% jsonFilePath = '../data/cyc02/MS01_task06_v2_fair_20231030_135025.json';  % fair
+% jsonFilePath = '../data/cyc02/MS01_task06_v2_right_20231030_153745.json';  % right biased
+% jsonFilePath = '../data/cyc02/MS01_task06_v2_left_20231030_154843.json';  % left biased
+
+% jsonFilePath = '../data/cyc02/HA01_task06_v2_fair_20231031_111134.json';  % fair
+
+% jsonFilePath = '../data/cyc02/AS01_task06_v2_fair_20231031_113747.json';  % fair
+
+
+% Open the JSON file and read its content
+fileID = fopen(jsonFilePath);
+jsonContent = fread(fileID, '*char')';
+fclose(fileID);
+
+% Parse the JSON content
+jsonData = jsondecode(jsonContent);
+
+% convert structure to arrays
+dir = cell2mat(struct2cell(jsonData.postflash_dir));
+clk_xerr = cell2mat(struct2cell(jsonData.click_xerr));
+clk_yerr = cell2mat(struct2cell(jsonData.click_yerr));
+
+% create data cell
+err_cell{1} = clk_xerr(dir<0);
+err_cell{2} = clk_yerr(dir<0);
+err_cell{3} = clk_xerr(dir>0);
+err_cell{4} = clk_yerr(dir>0);
+
+% err_mat = cell2mat(err_cell);
+
+labels = {'Left','Right'};
+ntypes = numel(labels);
+
+%% 2D plot
+figure('units','normalized','outerposition',[.1 .3 .3 .5])
+hold on
+
+alpha = .25;
+
+h1 = scatter(err_cell{:,1},err_cell{:,2},'r','<','fill','markerfacealpha',alpha);
+h2 = scatter(err_cell{:,3},err_cell{:,4},'b','>','fill','markerfacealpha',alpha);
+
+h3 = scatter(mean(err_cell{:,1}),mean(err_cell{:,2}),'r','o','fill');
+h4 = scatter(mean(err_cell{:,3}),mean(err_cell{:,4}),'b','o','fill');
+
+xline(0)
+% xlim([-1 1] * 1.75)
+xticks(-5:.2:5)
+xlabel 'Horizontal click error (dva)'
+
+yline(0)
+% ylim([-1 1] * 1.75)
+yticks(-5:.2:5)
+ylabel 'Vertical click error (dva)'
+
+legend([h1 h2 h3 h4],[labels, {'avg Left','avg Right'}])
+
+grid on
+cleanplot
+
+%%
+function scatterbar(A,marksz,color)
+% A: a cell of cetegories
+
+ncat    = numel(A); % number of categories
+stdx    = .05; % standard deviation of scatters in each category
+linelm  = .3; % line length for median
+alpha = .3;
+
+hold on
+for icat = 1:ncat    
+    rng default
+    n = numel(A{icat});
+    x = randn(n,1)*stdx + icat;
+    
+    scatter(x,A{icat},marksz,color,'o','fill','markerfacealpha',alpha);
+    line([icat-linelm icat+linelm],[nanmedian(A{icat}) nanmedian(A{icat})],...
+        'color',color,'linewidth',2);
+end
+
+xlim([0 ncat+1])
+set(gca,'xtick',1:ncat)
+end

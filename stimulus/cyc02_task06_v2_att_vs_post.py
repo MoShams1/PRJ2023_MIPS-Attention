115621e85ee49a61e1b0ff049dfd88ac2d531861
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-11-03 11:10:54 -0400

    ran AD01

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -53,7 +53,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'AB01'
+subID = 'AD01'
 nrep = 50
 ndir = 2
 ntrs = nrep * ndir
@@ -104,7 +104,7 @@ motion_cycle_dur = REF_RATE  # [frames]
 preflash_dist = 5  # dva
 postflash_dist = 5  # dva
 npos = int(motion_cycle_dur / frame_repeat / 2)
-bias_factor = 0.2  # probability of leftward post-flash motion
+bias_factor = 0.5  # probability of leftward post-flash motion
 
 # probe
 probe_rad = .15

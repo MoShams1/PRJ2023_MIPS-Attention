78fcb74570fbd4759aa99958c686b344469b345c
Mohammad Shams <m.shamsahmar@gmail.com>
2023-11-03 12:01:31 -0400

    new analyses plus file name modification

--- analysis/cyc02_task06_v2_individuals.m
+++ analysis/cyc02_task06_v2_individuals.m
@@ -15,10 +15,12 @@ close all
 % jsonFilePath = '../data/cyc02/AB01_task06_v2_fair_20231101_133455.json';  % fair
 jsonFilePath = '../data/cyc02/AB01_task06_v2_right_20231101_144331.json';  % right
 
-% jsonFilePath = '../data/cyc02/MM01_task06_v2_fair_20231101_141135.json';  % fair
+% jsonFilePath = '../data/cyc02/MM01_task06_v2_fair_20231101_141135.json';  % fair1
 % jsonFilePath = '../data/cyc02/MM01_task06_v2_fair_20231101_142041.json';  % fair
 
+% jsonFilePath = '../data/cyc02/AD01_task06_v2_fair_20231103_110225.json';  % fair
 
+% jsonFilePath = '../data/cyc02/RP01_task06_v2_fair_20231103_113807.json';  % fair
 
 % Open the JSON file and read its content
 fileID = fopen(jsonFilePath);
@@ -39,8 +41,6 @@ err_cell{2} = clk_yerr(dir<0);
 err_cell{3} = clk_xerr(dir>0);
 err_cell{4} = clk_yerr(dir>0);
 
-% err_mat = cell2mat(err_cell);
-
 labels = {'Left','Right'};
 ntypes = numel(labels);
 
@@ -57,12 +57,10 @@ h3 = scatter(mean(err_cell{:,1}),mean(err_cell{:,2}),'r','o','fill');
 h4 = scatter(mean(err_cell{:,3}),mean(err_cell{:,4}),'b','o','fill');
 
 xline(0)
-% xlim([-1 1] * 1.75)
 xticks(-5:.2:5)
 xlabel 'Horizontal click error (dva)'
 
 yline(0)
-% ylim([-1 1] * 1.75)
 yticks(-5:.2:5)
 ylabel 'Vertical click error (dva)'
 
@@ -71,26 +69,3 @@ legend([h1 h2 h3 h4],[labels, {'avg Left','avg Right'}])
 grid on
 cleanplot
 
-%%
-function scatterbar(A,marksz,color)
-% A: a cell of cetegories
-
-ncat    = numel(A); % number of categories
-stdx    = .05; % standard deviation of scatters in each category
-linelm  = .3; % line length for median
-alpha = .3;
-
-hold on
-for icat = 1:ncat    
-    rng default
-    n = numel(A{icat});
-    x = randn(n,1)*stdx + icat;
-    
-    scatter(x,A{icat},marksz,color,'o','fill','markerfacealpha',alpha);
-    line([icat-linelm icat+linelm],[nanmedian(A{icat}) nanmedian(A{icat})],...
-        'color',color,'linewidth',2);
-end
-
-xlim([0 ncat+1])
-set(gca,'xtick',1:ncat)
-end

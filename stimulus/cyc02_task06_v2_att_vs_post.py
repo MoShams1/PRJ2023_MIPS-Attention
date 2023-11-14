a1866e0d500cc7316e60a8c61360a71f6656b7da
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-11-14 12:56:57 -0500

    ran Hawa (right)

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -53,7 +53,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'NG01'
+subID = 'HA01'
 nrep = 50
 ndir = 2
 ntrs = nrep * ndir

b3ee7c07b2152b5049ee06606dc2ce8f38a1bf6d
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-11-15 17:57:53 -0500

    ran Jasman (left)

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -53,7 +53,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'NG01'
+subID = 'JK01'
 cnd = 'left'
 nrep = 50
 ndir = 2

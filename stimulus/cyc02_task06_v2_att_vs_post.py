86dd9164796bec74dd6405f672e3775f4ab567f3
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-11-16 13:32:07 -0500

    ran Yael (fair)

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -53,8 +53,8 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'JK01'
-cnd = 'left'
+subID = 'YG01'
+cnd = 'fair'
 nrep = 50
 ndir = 2
 ntrs = nrep * ndir

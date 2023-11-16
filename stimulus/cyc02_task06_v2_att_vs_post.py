cac5c7c8f00afaf1b2e77f67cd929540e0b29b45
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-11-16 13:49:08 -0500

    ran Zoha (fair)

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -53,7 +53,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'YG01'
+subID = 'test'
 cnd = 'fair'
 nrep = 50
 ndir = 2

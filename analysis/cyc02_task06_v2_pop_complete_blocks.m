c2edec88e8f4dc8ba02bcb24632cd5035a0988db
york <m.shamsahmar@gmail.com>
2023-11-17 18:52:04 -0500

    new analyses

--- analysis/cyc02_task06_v2_pop_complete_blocks.m
+++ analysis/cyc02_task06_v2_pop_complete_blocks.m
@@ -7,14 +7,14 @@ file_dir{1} = dir('../data/cyc02/complete/*left*');
 file_dir{2} = dir('../data/cyc02/complete/*fair*');
 file_dir{3} = dir('../data/cyc02/complete/*right*');
 
-k1_list = [0, .5];
-k2_list = [.5, 1];
+k1_list = [0, .7];
+k2_list = [.3, 1];
 
 c_all = lines(7);
 c_map = [c_all(5,:); c_all(2,:)];
 color_map = [1, 0, 0; 0, 0, 1];
 
-label_list = {'first half', 'second half'};
+label_list = {'First third', 'Last third'};
 
 for ik = 1:2
     for ifile = 1:3
@@ -52,7 +52,7 @@ for ik = 1:2
         end
     end
 
-    %%
+    %
     gray = .5 * ones(1,3);
 
     rM_rB = errx_rM(:,3);
@@ -105,11 +105,13 @@ for ik = 1:2
 
     scatterbar(Dif_mat, 20, color_map)
     
+    title(label_list{(ik)})
     xlim([.5 2.5])
     xticks(1:2)
-    xticklabels({'20%','80%'})
+    xticklabels({'0.2','0.8'})
+    xlabel 'Motion direction probability'
     
-    ylabel 'Click error difference rel. fair condition (dva)'
+    ylabel({'Click error difference (dva)','[biased - unbiased]'})
     ylim([-1, 1])
     yticks(-1:.2:1)
     yline(0)
@@ -122,25 +124,26 @@ for ik = 1:2
     pval_unlikely = signrank(Dif_mat(:,1));
     diff_likely = median(Dif_mat(:,2));
     pval_likely = signrank(Dif_mat(:,2));
-    fprintf('20%: median= %5.2f dva | p-val= %5.2f\n', diff_unlikely, pval_unlikely)
-    fprintf('80%: median= %5.2f dva | p-val= %5.2f\n', diff_likely, pval_likely)
+    fprintf('20%%: median= %5.2f dva | p-val= %5.2f\n', diff_unlikely, pval_unlikely)
+    fprintf('80%%: median= %5.2f dva | p-val= %5.2f\n', diff_likely, pval_likely)
     disp('===')
 
 end
 
-%%
+%
 figure(1)
 xlim([.5 3.5])
 xticks(1:3)
 xticklabels({'0.2','0.5','0.8'})
 xlabel 'Motion direction probability'
 
-ylabel 'Click error (dva)'
+ylabel 'Click error in the direction of motion (dva)'
 yticks(0:.1:1)
 yline(0)
+ylim([-.1, .4])
 
-text(1,.32,'Second half','color',c_map(2,:))
-text(1,.335,'First half','color',c_map(1,:))
+text(1,.32,'Last third','color',c_map(2,:))
+text(1,.335,'First third','color',c_map(1,:))
 text(1,.3,['N = ',num2str(size(full_mat,1))],'color','k')
 
 cleanplot
@@ -151,7 +154,7 @@ if ~iscell(A)
     A = mat2cell(A,size(A,1),ones(1,size(A,2)));
 end
 ncat    = numel(A); % number of categories
-stdx    = .05; % standard deviation of scatters in each category
+stdx    = .025; % standard deviation of scatters in each category
 linelm  = .3; % line length for median
 alpha   = .3;
 
@@ -160,10 +163,18 @@ for icat = 1:ncat
     rng default
     n = numel(A{icat});
     x = randn(n,1)*stdx + icat;
+    
+    med_A = median(A{icat});
+    iq25_A = prctile(A{icat},25);
+    iq75_A = prctile(A{icat},75);
 
     scatter(x,A{icat},marksz,color(icat,:),'o','fill','markerfacealpha',alpha);
-    line([icat-linelm icat+linelm],[nanmedian(A{icat}) nanmedian(A{icat})],...
+    line([icat-linelm icat+linelm], [med_A med_A],...
         'color',color(icat,:),'linewidth',2);
+    line([icat-linelm icat+linelm], [iq25_A iq25_A],...
+        'color',color(icat,:),'linewidth',.5);
+    line([icat-linelm icat+linelm], [iq75_A iq75_A],...
+        'color',color(icat,:),'linewidth',.5);
 end
 
 xlim([0 ncat+1])

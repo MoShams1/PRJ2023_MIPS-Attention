6032a32906756ba638adb0513c3d1098e18d78c9
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-11-21 12:02:49 -0500

    ran Nikan

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -53,7 +53,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'OS01'
+subID = 'NM01'
 cnd = 'left'
 nrep = 50
 ndir = 2

50cbca089af58b8503bad87fe31a02f8024e9787
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-11-21 16:52:23 -0500

    ran Feryal

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -53,7 +53,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'CT01'
+subID = 'FM01'
 cnd = 'fair'
 nrep = 50
 ndir = 2

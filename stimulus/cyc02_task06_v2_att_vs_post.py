3ec4c8c7cf404f480fb9a14a72f1c385905c1056
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-11-22 11:17:08 -0500

    ran Aurore

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -53,8 +53,8 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'MS01'
-cnd = 'right'
+subID = 'AM01'
+cnd = 'left'
 nrep = 50
 ndir = 2
 ntrs = nrep * ndir

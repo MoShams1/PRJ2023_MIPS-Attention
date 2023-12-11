18cd553a0fbe44f2504371977ff005d780a2bfc3
shams_via_kvnl <m.shamsahmar@gmail.com>
2023-12-11 11:19:27 -0500

    demo to Aurore

--- stimulus/cyc02_task06_v2_att_vs_post.py
+++ stimulus/cyc02_task06_v2_att_vs_post.py
@@ -53,7 +53,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = 'CH01'
+subID = 'test'
 cnd = 'right'
 nrep = 50
 ndir = 2

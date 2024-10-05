06ea0e8fb1c089bb440b490bc3589aec487f6821
york <m.shamsahmar@gmail.com>
2024-10-05 18:02:43 -0400

    tons of file transfer

--- analysis/cyc05_exp01_insepct_individuals.m
+++ analysis/cyc05_exp01_insepct_individuals.m
@@ -1,13 +1,13 @@
 clc
 clear
-% close all
+close all
 
 % Specify the path to the JSON files
 
-file_dir = dir('../data/cyc05/*task0_09*');
+file_dir = dir('../data/cyc05/*exp01*');
 nsub = numel(file_dir);
 
-isub = 2;
+isub = 1;
 
 disp(['Subject: ', file_dir(isub).name(end-8:end-5)])
 
@@ -26,9 +26,11 @@ jsonData = jsondecode(jsonContent);
 %%% convert structure to arrays
 ntrials = length(cell2mat(struct2cell(jsonData.probe2bar_dva)));
 bar_dir = cell2mat(struct2cell(jsonData.motion_dir));
-distance = cell2mat(struct2cell(jsonData.probe2bar_dva));
+
+probe2bar = cell2mat(struct2cell(jsonData.probe2bar_dva));
+xoffset = cell2mat(struct2cell(jsonData.bar_xoffset));
 probe_pos = reshape(cell2mat(struct2cell(jsonData.probe_pos)), 2, ntrials)';
-probe_xoffset = probe_pos(:,1);
+
 click_pos = reshape(cell2mat(struct2cell(jsonData.click_pos)), 2, ntrials)';
 click_xpos = click_pos(:,1);
 click_xerr = cell2mat(struct2cell(jsonData.click_xerr));
@@ -37,30 +39,30 @@ click_xerr(bar_dir == -1) = -click_xerr(bar_dir == -1);
 cmap = lines(7);
 
 %% Position shift vs. Bar-Probe distance
-distance_base = unique(distance)';
+p2b_base = unique(probe2bar)';
 
-distance_count = 0;
-for idistance = distance_base
-    distance_count = distance_count+1;    
-    ind = distance == idistance;    
-    err_mat(:,distance_count) = click_xerr(ind);
+p2b_count = 0;
+for ip2b = p2b_base
+    p2b_count = p2b_count+1;    
+    ind = probe2bar == ip2b;    
+    err_mat(:,p2b_count) = click_xerr(ind);
 end
 
 figure('units','inches','outerposition',[0, 0, 5, 5])
-x = distance_base;
-y = mean(err_mat);
+x = p2b_base;
+y = median(err_mat);
 e = SE(err_mat);
 errorbar(x, y, e, ...
     'o-','linewidth',2)
 
-% xticks(-4:.5:4)
+xticks(-4:1:4)
 xlim([-4.5 4.5])
-xlabel 'Probe-Bar distance (dva)'
+xlabel({'Probe-Bar distance (dva)', '(in direction of motion)'})
 xline(0)
 
-% yticks(-5:.5:5)
-% ylim([-1 1.5])
-ylabel 'Position shift in direction of Bar motion (dva)'
+yticks(-5:.25:5)
+ylim([-.2 1.2])
+ylabel({'Position shift (dva)', '(in direction of motion)'})
 yline(0)
 
 cleanplot

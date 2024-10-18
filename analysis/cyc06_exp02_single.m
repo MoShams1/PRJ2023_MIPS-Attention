fdc5d5234fe2be1f6f1d0b7eafe8d1b52a56252a
york <m.shamsahmar@gmail.com>
2024-10-18 11:58:26 -0400

    analyzed 0003 exp02

---
+++ analysis/cyc06_exp02_single.m
@@ -0,0 +1,74 @@
+clc
+clear
+close all
+
+% Specify the path to the JSON files
+
+file_dir = dir('../data/cyc06/*exp02*');
+nsub = numel(file_dir);
+
+isub = 1;
+
+subj_id = file_dir(isub).name(end-8:end-5);
+disp(['Subject: ', file_dir(isub).name(end-8:end-5)])
+
+% Specify the path to the JSON file
+jsonFilePath = fullfile(file_dir(isub).folder,file_dir(isub).name);
+
+% Open the JSON file and read its content
+fileID = fopen(jsonFilePath);
+jsonContent = fread(fileID, '*char')';
+fclose(fileID);
+
+% Parse the JSON content
+jsonData = jsondecode(jsonContent);
+
+
+%%% convert structure to arrays
+ntrials = length(cell2mat(struct2cell(jsonData.probe2bar_rel2motion)));
+bar_dir = cell2mat(struct2cell(jsonData.motion_dir));
+
+probe2bar = cell2mat(struct2cell(jsonData.probe2bar_rel2motion));
+probe_pos = repmat([0,5], ntrials, 1);
+click_pos = reshape(cell2mat(struct2cell(jsonData.click_pos)), 2, ntrials)';
+click_xpos = click_pos(:,1);
+click_xerr = cell2mat(struct2cell(jsonData.click_xerr));
+click_xerr(bar_dir == -1) = -click_xerr(bar_dir == -1);
+
+p2b_base = unique(probe2bar)';
+err_mat = nan(24,numel(p2b_base));
+p2b_count = 0;
+for ip2b = p2b_base
+    p2b_count = p2b_count+1;
+    ind_p2b = probe2bar == ip2b;
+    err_mat(1:sum(ind_p2b),p2b_count) = click_xerr(ind_p2b);
+end
+
+
+%% Figure variables
+y_limit = [-.5 1];
+cmap = lines(7);
+
+%% Position shift vs. Bar-Probe distance
+figure('units','inches','outerposition',[0, 0, 5, 5])
+hold on
+
+x = p2b_base;
+y = nanmean(err_mat);
+e = SE(err_mat);
+errorbar(x, y, e, ...
+    'o-','linewidth',2,'color','k')
+
+
+xticks(-4:1:4)
+xlim([-4.5 4.5])
+xlabel({'Probe-Bar distance (dva)', '(in direction of motion)'})
+xline(0)
+
+yticks(-5:.2:5)
+ylim(y_limit)
+ylabel({'Position shift (dva)', '(in direction of motion)'})
+yline(0)
+
+title(['Subject: ', subj_id])
+cleanplot

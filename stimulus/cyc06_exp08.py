c17a722f75a9b5d66f9d703d792127d3ff0c18d0
shams_via_kvnl <m.shamsahmar@gmail.com>
2025-04-06 21:16:47 -0400

    ran 0002

--- stimulus/cyc06_exp08.py
+++ stimulus/cyc06_exp08.py
@@ -69,7 +69,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 # ----------------------------------------------------------------------------
 # /// INSERT SESSION'S META DATA ///
 
-subID = '0001'  # put 'test' for a test run
+subID = '0002'  # put 'test' for a test run
 slow_coeff = 1
 
 # ----------------------------------------------------------------------------
